ad7def4428f980764cc27ce568204f58c34ee833
Tatamo <nsorder@gmail.com>
2017-09-23 20:59:15 +0900

    fix template

--- template/ts/main.ts
+++ template/ts/main.ts
@@ -2,9 +2,10 @@ declare function require(x: string): any;
 
 var input = require("fs").readFileSync("/dev/stdin", "utf8");
 input = input.split("\n");
+const n = +input[0];
 const a = [];
 for(let i=0;i<n;i++){
-	a.push(input[i].split(" ").map((x:string):number=>+x));
+	a.push(input[i+1].split(" ").map((x:string):number=>+x));
 }
 
 console.log(input);
