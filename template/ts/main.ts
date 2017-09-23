06b2f5b81f9c852234759c0798c79ba09793084f
Tatamo <nsorder@gmail.com>
2017-09-24 01:32:32 +0900

    improve template

--- template/ts/main.ts
+++ template/ts/main.ts
@@ -1,8 +1,7 @@
-declare function require(x: string): any;
-
-var input = require("fs").readFileSync("/dev/stdin", "utf8");
-input = input.split("\n");
+import * as fs from "fs";
+let input = (fs.readFileSync("/dev/stdin", "utf8") as string).split("\n");
 const n = +input[0];
+// const [n] = input[0].split(" ").map((x: string): number => +x);
 const a = [];
 for(let i=0;i<n;i++){
 	a.push(input[i+1].split(" ").map((x:string):number=>+x));
