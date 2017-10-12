cdf5810cfac0078105fc7800d1bfdbe1c9e29868
Tatamo <nsorder@gmail.com>
2017-10-12 14:00:34 +0900

    AC: CODEFES2017 qualB C (re-solve)

--- contest/atcoder/code-festival-2017-qualb/c.ts
+++ contest/atcoder/code-festival-2017-qualb/c.ts
@@ -13,6 +13,7 @@ export interface ListGraphConstructorOption {
 	mutual_edge?: boolean;
 }
 
+
 export class ListGraph {
 	private list: ListGraphArray;
 	private _edges: number;
@@ -50,6 +51,7 @@ export class ListGraph {
 		}
 	}
 
+	// TODO: 無向グラフに
 	private initArray(input: ListGraphArray) {
 		this._edges = 0;
 		this.list = new Array<Array<ListGraphEdge>>(input.length);
@@ -96,129 +98,44 @@ export class ListGraph {
 		return result;
 	}
 
-	dijkstra(start: number): { cost: Array<number>, prev: Array<number> } {
-		const size = this.list.length;
-		const visited = new Array<boolean>(size);
-		for (let i = 0; i < size; i++) visited[i] = false;
-		const min_cost = new Array<number>(size); // startからの最短距離
-		min_cost.fill(Infinity);
-		min_cost[start] = 0;
-		const prev = new Array<number>(size); // 直前に訪れるノード
-		prev.fill(-1);
-		prev[start] = start;
 
-		while (true) {
-			let node = -1;
-			for (let i = 0; i < size; i++) {
-				if (!visited[i] && (node == -1 || min_cost[i] < min_cost[node])) node = i;
-			}
-			if (node == -1) break;
-			visited[node] = true;
-			for (const {to, cost} of this.list[node]) {
-				if (cost < 0) {
-					throw Error("edge cannot have a cost that is less than 0");
-				}
-				if (min_cost[node] + cost < min_cost[to]) {
-					min_cost[to] = min_cost[node] + cost;
-					prev[to] = node;
-				}
-			}
-		}
-		return {cost: min_cost, prev};
-	}
-
-	// O(VE)
-	bellmanFord(start: number): { has_negative_loop: false, cost: Array<number>, prev: Array<number> } | { has_negative_loop: true, cost: Array<number | null>, prev: Array<number | null> } {
+	// 2部グラフ判定
+	// TODO: 有向グラフ対応
+	isBigraph(): { result: false } | { result: true, colors: Array<-1 | 1> } {
 		const size = this.list.length;
-		const min_cost = new Array<number>(size); // startからの最短距離
-		min_cost.fill(Infinity);
-		min_cost[start] = 0;
-		const prev = new Array<number>(size); // 直前に訪れるノード
-		prev.fill(-1);
-		prev[start] = start;
-		const correct = new Array<boolean>(size); // 正しい最短経路が求まっているかどうか
-		correct.fill(true);
-		let loop_count = 0;
-		let flg_contain_negative = false;
-		let flg_update = true;
-		while (flg_update) {
-			flg_update = false;
-			for (let from = 0; from < size; from++) {
-				for (const {to, cost} of this.list[from]) {
-					if (min_cost[from] != Infinity && min_cost[to] > min_cost[from] + cost) {
-						min_cost[to] = min_cost[from] + cost;
-						prev[to] = from;
-						flg_update = true;
-						if (flg_contain_negative) {
-							// V回目以降に最短距離が更新されるなら、負閉路を通っているので正しくない
-							correct[to] = false;
-						}
+		const color = new Array(size + 1).fill(0);
+		const stack = [];
+		for (let i = 0; i < size; i++) {
+			if (color[i] != 0) continue;
+			color[i] = 1;
+			stack.push(i);
+			while (stack.length > 0) {
+				const node: number = stack.pop()!;
+				for (const edge of this.list[node]) {
+					if (color[edge.to] == color[node]) return {result: false};
+					if (color[edge.to] == 0) {
+						color[edge.to] = -color[node];
+						stack.push(edge.to);
 					}
 				}
 			}
-			loop_count += 1;
-			if (loop_count == size) {
-				// 頂点の数だけ更新してもループがとまらない -> 負閉路が存在する
-				flg_contain_negative = true;
-			}
-			if (loop_count == size * 2) break; // 頂点の数*2回より多くループする意味はない
 		}
-		if (flg_contain_negative) {
-			// 正しく最短距離が求められない頂点の結果はnullとする
-			const min_cost_with_undefined: Array<number | null> = min_cost;
-			const prev_with_undefined: Array<number | null> = prev;
-			for (let i = 0; i < size; i++) {
-				if (!correct[i]) {
-					min_cost_with_undefined[i] = prev_with_undefined[i] = null;
-				}
-			}
-			return {has_negative_loop: true, cost: min_cost_with_undefined, prev: prev_with_undefined};
-		}
-		else {
-			return {has_negative_loop: false, cost: min_cost, prev};
-		}
+		return {result: true, colors: color};
 	}
 }
+
 
 let input = (fs.readFileSync("/dev/stdin", "utf8") as string).split("\n");
 const [n, m] = input[0].split(" ").map((x: string): number => +x);
 const g = new ListGraph(input, m, 1, {mutual_edge: true});
 
 // 2部グラフ判定
-const color: Array<number> = new Array(n + 1).fill(0);
-/*
-// スタックオーバーフローする
-const dfs = (node: number, c: number): boolean => {
-	if (node < 0 || node > n) return true;
-	color[node] = c;
-	for (const edge of g.getArray(true)[node]) {
-		if (color[edge.to] == c) return false;
-		if (color[edge.to] == 0 && !dfs(edge.to, -c)) return false;
-	}
-	return true;
-};
-*/
-
-const dfs = (n: number): boolean => {
-	const stack = [n];
-	color[n] = 1;
-	while (stack.length > 0) {
-		const node = stack.pop()!;
-		for (const edge of g.getArray(true)[node]) {
-			if (color[edge.to] == color[node]) return false;
-			if(color[edge.to] == 0){
-				color[edge.to] = -color[node];
-				stack.push(edge.to);
-			}
-		}
-	}
-	return true;
-};
-
-if (dfs(1)) {
+const tmp = g.isBigraph();
+if (tmp.result) {
 	let b = 0;
 	let w = 0;
-	for (const c of color) {
+	for (let i = 1; i <= n; i++) {
+		let c = tmp.colors[i];
 		if (c == 1) b += 1;
 		else if (c == -1) w += 1;
 	}
