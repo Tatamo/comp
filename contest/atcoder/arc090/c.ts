91b8069b1f09a5d828646bf3a0af8cd1f6229c27
Tatamo <nsorder@gmail.com>
2018-01-29 02:50:34 +0900

    AC:ARC090 C

---
+++ contest/atcoder/arc090/c.ts
@@ -0,0 +1,26 @@
+import * as fs from "fs";
+
+let input = (fs.readFileSync("/dev/stdin", "utf8") as string).split("\n");
+
+const n = +input[0];
+const a0 = input[1].split(" ").map((x: string): number => +x);
+const a1 = input[2].split(" ").map((x: string): number => +x);
+
+const s0: number[] = [];
+for (let i = 0; i < n; i++) {
+	if (i === 0) s0[i] = a0[i];
+	else s0[i] = a0[i] + s0[i - 1];
+}
+
+const s1: number[] = [];
+for (let i = n - 1; i >= 0; i--) {
+	if (i === n - 1) s1[i] = a1[i];
+	else s1[i] = a1[i] + s1[i + 1];
+}
+
+let result = -1;
+for (let i = 0; i < n; i++) {
+	const sum = s0[i] + s1[i];
+	if (sum > result) result = sum;
+}
+console.log(result);
