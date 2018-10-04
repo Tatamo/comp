0a51aa4c35b6a245a2e6b2d7bce141106cb4c596
Tatamo <nsorder@gmail.com>
2018-10-04 16:52:04 +0900

    fix .gitignore

--- contest/atcoder/abs/abc086a.fs
+++ contest/atcoder/abs/abc086a.fs
@@ -1,6 +1,5 @@
-let input f = System.Console.ReadLine() |> f
-let inputs f = System.Console.ReadLine().Split() |> Array.map f
- 
+let inline input f = System.Console.ReadLine() |> f
+let inline inputs f = System.Console.ReadLine().Split() |> Array.map f
+
 let [|a;b|] = inputs int
 printfn "%s\n" (if a*b%2=0 then "Even" else "Odd")
-
